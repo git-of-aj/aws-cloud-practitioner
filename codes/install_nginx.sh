93a92a842e9348196f5e354a75662051bf48e5d9
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-03-19 20:59:45 +0530

    install apache2 with /video directory

--- codes/install_nginx.sh
+++ codes/install_nginx.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Update package list and install Apache web server
 sudo apt update -y
@@ -6,5 +6,7 @@ sudo apt install apache2 -y
 
 # Create directory for video page and index file
 sudo mkdir -p /var/www/html/video
-echo "<iframe width="560" height="315" src="https://www.youtube.com/embed/teHR00DpX2E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>" > index.html
+wget https://raw.githubusercontent.com/Ananyojha/AZ-104-Training/main/index.html
+sudo cp ./index.html /var/www/html/video/index.html
+
 
